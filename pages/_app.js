b100bad66b7e515b9c901eaa36a19c860682385e
Himanshu raj <115911540+hr7657316@users.noreply.github.com>
2024-03-02 23:23:02 +0530

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '@/styles/globals.css'
+import {Toaster} from "react-hot-toast";
+
+export default function App({Component, pageProps}) {
+	return (
+		<>
+			<Component {...pageProps} />
+			<Toaster/>
+		</>
+	)
+}
---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import FileUpload from "@/components/FileUpload";
+import Intro from "@/components/Intro";
+import Head from 'next/head'
+import {useState} from "react";
+
+export default function Home() {
+	const [transcript, setTranscript] = useState('')
+	return (
+		<div className={"bg-[url('/bg.jpg')] object-contain"}>
+			<Head>
+				<title>transcript - Convert Audio To Text</title>
+			</Head>
+
+			<main className={`w-full h-screen`}>
+				<div className={"max-w-5xl mx-auto text-primary"}>
+					{/*header*/}
+					<h1 className={"px-5 lg:px-0 py-4 text-5xl font-bold text-primary"}> Transcriber</h1>
+
+					{/*content*/}
+					<div className={"flex flex-col mt-5 px-5 lg:px-0 h-[calc(100vh-170px)] min-h-[calc(100vh-170px)]"}>
+
+						{/*intro*/}
+						<Intro/>
+
+						{/*file upload*/}
+						<div className={"w-full w-9/12"}><FileUpload setTranscript={setTranscript}/></div>
+
+						{/*transcript*/}
+						<h2 className={"text-lg mb-2"}>Transcript:</h2>
+						<div className={"border grow overflow-auto bg-[#f7f5f5] text-black p-3 rounded"}>
+							{
+								transcript
+									? transcript
+									: <div className={"flex justify-center items-center h-full bg-opacity-0"}>Nothing here</div>
+
+							}
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	)
+}
